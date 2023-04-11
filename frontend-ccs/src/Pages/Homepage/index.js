ce0111d870d4df59ea47e1a70ade6ee65c8725ea
FelipeGomes-20 <felipe.gomes@sptech.school>
2023-04-11 20:46:14 -0300

    Bug da tela de contato corrigido

--- frontend-ccs/src/Pages/Homepage/index.js
+++ frontend-ccs/src/Pages/Homepage/index.js
@@ -4,17 +4,26 @@ import Mvv from './Componentes/Mvv';
 import SobreNos from './Componentes/SobreNos';
 import './Homepage.css'
 import Footer from './Componentes/Footer';
-import Contato from './Componentes/Contato';
+import Formulario from './Componentes/Formulario';
+import { useState } from 'react';
 
 const Homepage = () => {
+
+    const [colaboradores, setColaboradores] = useState([])
+
+    const aoNovoColaboradorAdicionado = (colaborador) => {
+      setColaboradores([...colaboradores, colaborador])
+    }
+
+
     return (
         <>
             <Header />
-            <div className='suportButton'><a href='#'><img src='/imgs/icons/apoio-suporte.png' /></a></div>
+            <div className='suportButton'><img src='/imgs/icons/apoio-suporte.png' /></div>
             <SobreNos />
             <Fotos />
             <Mvv />
-            <Contato />
+            <Formulario  aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
             <Footer />
         </>
     );
