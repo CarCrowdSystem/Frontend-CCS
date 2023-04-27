46f90f9eb7e87dd30a9e796fdc2973b4faacafc3
FelipeGomes-20 <felipe.gomes@sptech.school>
2023-04-27 17:07:12 -0300

    feito hover nas fotos da index

--- frontend-ccs/src/pages/home/Componentes/Fotos/index.js
+++ frontend-ccs/src/pages/home/Componentes/Fotos/index.js
@@ -3,22 +3,44 @@ import './Fotos.css'
 const Fotos = () => {
     return (
         <div className='fotos'>
-            <div className='imgLeft'>
 
-                <img src='/imgs/fotoIndex1.png' />
-                <div class="overlay">
-                    <div class="text">Hello World</div>
+            <div className='container'>
+                <img src='imgs/fotoIndex1.png' className='image' />
+                <div className='overlay'>
+                    <div className='text'>Analise seus lucros</div>
                 </div>
-                
             </div>
             <div className='imgRight'>
                 <div className='cima'>
-                    <img src='/imgs/lancer.jpg' />
-                    <img src='/imgs/fotoIndex3.png' />
+
+                    <div className='container'>
+                        <img src='imgs/lancer.jpg' className='image' />
+                        <div className='overlay'>
+                            <div className='text'>Gerencie os veículos</div>
+                        </div>
+                    </div>
+                    <div className='container'>
+                        <img src='imgs/fotoIndex3.png' className='image' />
+                        <div className='overlay'>
+                            <div className='text'>Agilize a retirada de veículos por mensagem</div>
+                        </div>
+                    </div>
+                    
                 </div>
+
                 <div className='baixo'>
-                    <img src='/imgs/fotoIndex5.png' />
-                    <img src='/imgs/fotoIndex6.jpg' />
+                    <div className='container'>
+                        <img src='imgs/fotoIndex5.png' className='image' />
+                        <div className='overlay'>
+                            <div className='text'>Mapeie as vagas</div>
+                        </div>
+                    </div>
+                    <div className='container'>
+                        <img src='imgs/fotoIndex6.jpg' className='image' />
+                        <div className='overlay'>
+                            <div className='text'>Analise dashboards para tomada de decisão</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
