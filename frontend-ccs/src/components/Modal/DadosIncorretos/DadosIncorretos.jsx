b71e32ac00bd5fa08bbf7be4f9f0ed10c99beac4
Lucas Penalva <99892425+lucasp-franca@users.noreply.github.com>
2023-06-07 21:50:11 -0300

    Add files via upload

---
+++ frontend-ccs/src/components/Modal/DadosIncorretos/DadosIncorretos.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DadosIncorretos.css'
+
+// import { Container } from './styles';
+
+function DadosIncorretos() {
+    return (
+        <>
+            <div className='div-dados-incorretos'>
+                <p className='texto-dados-incorretos'>Os dados inseridos são inválidos, tente novamente!</p>
+            </div>
+        </>
+      );
+}
+
+export default DadosIncorretos;
