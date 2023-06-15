87a13342533463b4eca1a034e86a036928e11ca1
FelipeGomes-20 <felipe.gomes@sptech.school>
2023-06-15 00:50:55 -0300

    organizando pastas para deploy

---
+++ src/components/Modal/DadosEstacionamentoAlteradosComSucesso/DadosEstacionamentoAlteradosComSucesso.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DadosEstacionamentoAlteradosComSucesso.css'
+
+// import { Container } from './styles';
+
+function DadosEstacionamentoAlteradosComSucesso() {
+    return (
+        <>
+            <div className='div-dados-estacionamento-alterados-com-sucesso'>
+                <p className='texto-dados-estacionamento-alterados-com-sucesso'>Dados do estacionamento alterados com sucesso!</p>
+            </div>
+        </>
+      );
+}
+
+export default DadosEstacionamentoAlteradosComSucesso;
