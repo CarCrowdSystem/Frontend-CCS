ccafa9ee5b37a94ad9f7c7f19764e8a27f4913fb
Lucas Penalva <99892425+lucasp-franca@users.noreply.github.com>
2023-10-28 00:49:42 -0300

    Criado tela de agendamentos

---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Home from "./pages/home/Home";
+import NotFound from "./pages/notFound/NotFound";
+import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Estacionamento from "./pages/estacionamento/Estacionamento";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Checkin from "./pages/checkin/Checkin";
+import Valores from "./pages/valores/Valores";
+import Historico from "./pages/historico/Historico";
+import Funcionarios from "./pages/funcionarios/Funcionarios";
+import CheckoutCliente from "./pages/dashboard/Componentes/CheckoutCliente";
+import Agendamentos from "./pages/agendamentos/Agendamentos";
+
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+
+function Rotas(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="*" element={<NotFound />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/estacionamento" element={<Estacionamento />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/checkin" element={<Checkin />} />
+                <Route path="/valores" element={<Valores />} />
+                <Route path="/historico" element={<Historico />} />
+                <Route path="/funcionarios" element={<Funcionarios />} />
+                <Route path="/checkout-cliente" element={<CheckoutCliente />} />
+                <Route path="/agendamentos" element={<Agendamentos />}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+export default Rotas;
