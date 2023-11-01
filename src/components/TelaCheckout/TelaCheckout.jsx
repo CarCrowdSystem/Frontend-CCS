54a7593de2715afa70f2d45e58a65a6acdcb33c6
Diogo <diogo.gomes@sptech.school>
2023-11-01 20:55:32 -0300

    tentando corrigir o web

---
+++ src/components/TelaCheckout/TelaCheckout.jsx
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import IconVoltarCheckout from "./icone_voltar_tela_checkout.png";
+import InfoClienteCheckout from "./InfoClienteCheckout/InfoClienteCheckout";
+import "./TelaCheckout.css";
+import api from "../../api.js";
+import Swal from "sweetalert2";
+
+const formTemplate = {
+  nome: "Sem checkouts",
+  telefone: " ",
+  andar: " ",
+  vaga: " ",
+  valor: " "
+};
+
+function TelaCheckout(props) {
+  var sessionIdEstacionamento = sessionStorage.getItem("ID_ESTACIONAMENTO");
+  const [hideDiv, setHideDiv] = useState(true);
+
+  const [checkouts, setCheckouts] = useState([]);
+
+  useEffect(() => {
+    setHideDiv(false);
+    console.log("useEffect");
+  }, []);
+
+  const hideDivOnClick = () => {
+    setHideDiv(true);
+    setTimeout(() => {
+      props.onClose();
+    }, 500);
+    console.log("hideDiv");
+  };
+
+  useEffect(() => {
+    pegaCheckouts()
+  }, []);
+
+  function pegarDadosDash(){
+    api
+    .get(`/historicos/pegar-dados-dash?id=${sessionIdEstacionamento}`)
+    .then((response) => {
+      sessionStorage.setItem("TOTAL_CHECKOUT_DIARIO", response.data.totalCheckoutDiario);
+      sessionStorage.setItem("TOTAL_FATURAMENTO", response.data.totalFaturamento);
+      const dadosVagas = Object.values(response.data.momentoVagas)
+      sessionStorage.setItem("MOMENTO_VAGAS", dadosVagas);
+
+      const qtdVagasLivres = response.data.momentoVagas.reduce((contador, momento) => {
+        if (momento.statusRegistro === 'Saida') {
+          contador++;
+        }
+        return contador;
+      }, 0);
+      sessionStorage.setItem("VAGAS_LIVRES", qtdVagasLivres)
+      const andaresSaida = new Set();
+      const andaresEntrada = new Set();
+
+      response.data.momentoVagas.forEach(momento => {
+        if (momento.statusRegistro === 'Saida') {
+          andaresSaida.add(momento.andar);
+        } else if (momento.statusRegistro === 'Entrada' && !andaresSaida.has(momento.andar)) {
+          andaresEntrada.add(momento.andar);
+        }
+      });
+      sessionStorage.setItem("ANDARES_SAIDA", andaresSaida.size);
+      sessionStorage.setItem("ANDARES_ENTRADA", andaresEntrada.size);
+    })
+    .catch((erro) => {
+        console.log("Deu b.o", erro)
+    })
+  }
+
+  function pegaCheckouts(){
+    api
+      .get(`/historicos/pegar-checkouts?id=${sessionIdEstacionamento}`)
+      .then((response) => {
+        console.log(response);
+        setCheckouts(response.data)
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }
+
+  function fazCheckout() {
+    api
+      .post(`/historicos/checkout?idVeiculo=${checkouts[0].fkVeiculo}`)
+      .then((response) => {
+        Swal.fire({
+          title: "Estacionamento atualizado!",
+          icon: "success",
+          confirmButtonColor: "#ff8000",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Ok",
+        });
+        console.log(response);
+        pegaCheckouts()
+        pegarDadosDash()
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+  }
+
+  return (
+    <>
+      <div className="div-desfoque"></div>
+      <div className={`tela-checkout ${hideDiv ? "sumir" : "aparecer"}`}>
+        <div className="div-checkout-pai">
+          <div className="icones-tela-checkout">
+            <button
+              onClick={hideDivOnClick}
+              className="botao-icon-voltar-chekcout"
+            >
+              <img
+                className="icon-voltar-chekcout"
+                src={IconVoltarCheckout}
+                alt="Voltar"
+              />
+            </button>
+            <img
+              className="icon-logo-chekcout"
+              src="/imgs/Group 39.png"
+              alt="Logo"
+            />
+          </div>
+          <div className="titulo-checkout">
+            <h1 className="title-checkout">Check-Out</h1>
+          </div>
+          <div className="container-itens-checkout-pai">
+            <div className="container-itens-checkout-filho">
+              <h2 className="elements-checkout">Cliente</h2>
+              <h2 className="elements-checkout">Telefone</h2>
+              <h2 className="elements-checkout">Andar</h2>
+              <h2 className="elements-checkout">Vaga</h2>
+              <h2 className="elements-checkout">Valor</h2>
+            </div>
+          </div>
+          <div className="container-conteudo-checkout-pai">
+            <div className="container-conteudo-checkout-filho">
+              {checkouts.map((checkout, i) => (
+                <React.Fragment key={i}>
+                  <InfoClienteCheckout
+                    nome={checkout.nome}
+                    telefone={checkout.telefone}
+                    andar={checkout.andar}
+                    vaga={checkout.vaga}
+                    valor={checkout.valor}
+                  />
+                </React.Fragment>
+              ))}
+            </div>
+            <div className="container-botao-checkout">
+              <button className="botao-checkout-tela-checkout" onClick={() => fazCheckout()}>
+                Check-out
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TelaCheckout;
---
+++ src/pages/dashboard/Componentes/Vaga/Vaga.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './Vaga.css';
+import Modal from '../../../../components/Modal/Modal';
+import DadosClienteCheckin from '../../../../components/Modal/DadosClienteCheckin/DadosClienteCheckin';
+import ClienteJaCadastrado from '../../../../components/Modal/ClienteJaCadastrado/ClienteJaCadastrado';
+
+const Vaga = ({numero, status, andar, andarSelecionado, idVaga}) => {
+  const [mostraModal, setMostraModal] = useState(false);
+  const [mostraModalCadastro, setMostraModalCadastro] = useState(false);
+
+  function mostrarModal() {
+    setMostraModal(true);
+    console.log(status);
+  }
+
+  function fecharModal() {
+    setMostraModal(false);
+    console.log("fechou");
+  }
+
+  const handleFecharModalCadastro = () => {
+    setMostraModal(true);
+    setMostraModalCadastro(false);
+  }
+
+  const handleMostrarModalCadastro = () => {
+    setMostraModal(false);
+    setMostraModalCadastro(true);
+  }
+
+  return (
+    <>
+      {mostraModal && (
+              <Modal onFecharModal={fecharModal}>
+                <ClienteJaCadastrado 
+                  func={handleMostrarModalCadastro} 
+                  idVaga={idVaga}
+                />
+              </Modal>
+            )}
+
+            {mostraModalCadastro && (
+              <Modal onFecharModal={handleFecharModalCadastro}>
+                <DadosClienteCheckin />
+              </Modal>
+            )}
+
+      {mostraModalCadastro && (
+        <Modal onFecharModal={handleFecharModalCadastro}>
+          <DadosClienteCheckin />
+        </Modal>
+      )}
+      <div className={andar === andarSelecionado ? "div-vagas-selecionadas" : "div-vagas-nao-selecionada"}>
+        <div
+          onClick={mostrarModal}
+          className={
+            status === "Saida"
+              ? "div-vaga-pai-checkin"
+              : "div-vaga-pai-checkin-indisponivel"
+          }
+        >
+          <div className="div-vaga-filho-checkin">
+            <p className={idVaga}>Vaga</p>
+            <p
+              className={
+                !status === "Saida"
+                  ? "numero-vaga-checkin-indisponivel"
+                  : "numero-vaga-checkin"
+              }
+            >
+              {numero}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Vaga;
